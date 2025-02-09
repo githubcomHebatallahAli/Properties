810ecab1ed9bbcfdfcef9cfb173f189266e43c81
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-02-09 19:51:08 +0200

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Vonage\Client;
+use Vonage\Client\Credentials\Basic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
     {
         Gate::define('manage_users', function($user) {
             return    Auth::guard('admin')->check();
+        });
+
+
+        $this->app->singleton(Client::class, function ($app) {
+            return new Client(new Basic(
+                config('vonage.api_key'),
+                config('vonage.api_secret')
+            ));
         });
     }
 }
--- config/services.php
+++ config/services.php
@@ -39,6 +39,8 @@ return [
     'api_key' => env('VONAGE_API_KEY'),
     'api_secret' => env('VONAGE_API_SECRET'),
     'from' => env('VONAGE_FROM'),
+    'application_id' => env('VONAGE_APPLICATION_ID'),
+    'private_key' => env('VONAGE_PRIVATE_KEY'),
 ],
 
 
